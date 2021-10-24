1e22539e217b88baea79983d1b0f5a9fc901b6c8
bayanmsl.bm <bayanmsl.bm@gmail.com>
2021-10-24 17:11:13 +0300

    digram

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
+import { AnimationsComponent } from './animations/animations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimationsComponent
   ],
   imports: [
     BrowserModule,
